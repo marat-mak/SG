f5e07e39d53736dea83bb983b4103c0eccad77eb
marat-mak <marat.mak@gmail.com>
2022-05-28 02:28:13 +0500

    new 1

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,12 @@
-field = [' '] * 9
+field = [[' '] * 3] * 3
+def show():
+    print()
+    print('     A   B   C   ')
+    print('   -------------')
+    for i, row in enumerate(field):
+        print(f'{i+1}  | {" | ".join(row)} |')
+        print('   -------------')
+    print()
+field[0][1]='x'
+field[1][1]='0'
+show()
